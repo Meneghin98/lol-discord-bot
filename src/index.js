752ce5aeafcf9311cf4519e6800c5794d1bc189d
Meneghin <simone.meneghin.98@outlook.it>
2021-07-11 21:55:00 +0200

    setup channel creation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import EnvUtils from './utils/EnvUtils.js'
-import Discord from 'discord.js'
+import {Client, Constants} from 'discord.js'
+const {PRESENCE_UPDATE, CLIENT_READY} = Constants.Events;
 
-const client = new Discord.Client();
+const client = new Client();
 
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+client.on(CLIENT_READY, () => {
+	client.guilds.cache.forEach(createBotChannel)
 });
 
+// client.on(PRESENCE_UPDATE, (a, b) => {
+// 	console.log(a, b);
+// });
 
 client.login(EnvUtils.getEnv(EnvUtils.DISCORD_BOT_TOKEN));
+
+function createBotChannel(guild){
+	const BOT_CHANNEL_NAME = "League bets";
+
+	const channels = guild.channels.cache.filter(c => c.name === BOT_CHANNEL_NAME)
+
+	if(channels.size == 0){
+		// guild.createChannel(BOT_CHANNEL_NAME, 'text')
+		guild.channels.create(BOT_CHANNEL_NAME)
+	} else {
+		console.log("channel already exists")
+	}
+}
